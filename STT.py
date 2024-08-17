e0b1a12844974732d1c35ebb89eff8a098c274ac
mateodlcc <mateodlcc@uc.cl>
2024-08-17 14:22:13 -0400

    Understanding spanish

--- STT.py
+++ STT.py
@@ -1,10 +1,6 @@
-# pip3 install speechrecognition
-# pip3 install pyaudio
-
 import speech_recognition as sr
 import pyttsx3
 
-# INTENTO 1 -----------------------------
 def listen():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -13,7 +9,7 @@ def listen():
     try:
         return r.recognize_google(audio, language="es-ES")
     except sr.UnknownValueError:
-        return "Could not understand audio"
+        return "Puedes repetirlo, por favor?"
     except sr.RequestError as e:
         return "Could not request results; {0}".format(e)
     
@@ -31,38 +27,3 @@ if __name__ == "__main__":
             break
 
 
-
-# INTENTO 2 -----------------------------
-# r = sr.Recognizer()
-
-# def record_text():
-#     while(1):
-#         try:
-#             with sr.Microphone() as source2:
-#                 r.adjust_for_ambient_noise(source2, duration=0.2)
-#                 print("Say something!")
-#                 audio2 = r.listen(source2, timeout=10, phrase_time_limit=5)
-#                 MyText = r.recognize_google(audio2)
-#                 return MyText
-            
-#         except sr.RequestError as e:
-#             print("Could not request results; {0}".format(e))
-
-#         except sr.UnknownValueError:
-#             print("unknown error occured")
-#             return "Could not understand audio"
-        
-#     return
-
-# def output_text(text):
-#     f = open("output.txt", "a")
-#     f.write(text + "\n")
-#     f.close()
-#     return
-
-# while(1):
-#     text = record_text()
-#     print("You said: ", text)
-#     output_text(text)
-#     if text == "exit":
-#         break
