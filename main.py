f446a0c98824d3ace6993f079ea16abc77a59ec8
rir001 <riveraldrv@gmail.com>
2024-08-16 22:59:29 -0400

    fix eyes

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import sys
-from time import sleep
-from random import randint
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
 from PyQt6.QtGui import QPainter, QBrush, QColor, QMouseEvent, QPixmap
 from PyQt6.QtCore import Qt, QPoint, QTimer
-from ultralytics import YOLO
-import cv2 
-import torch
+from face_detection import FaceDetection
+from PyQt6.QtCore import pyqtSignal
+import sys
 
 
 class Face(QWidget):
@@ -36,6 +33,13 @@ class Face(QWidget):
         self.speak_timer = QTimer(self)
         self.speak_timer.timeout.connect(self.speak)
         self.speak_timer.start(self.speak_step)
+
+        self.pose = QPoint(self.c_x, self.c_y)
+        self.target = QPoint(self.c_x, self.c_y)
+        self.face_timer = QTimer(self)
+        self.face_timer.timeout.connect(self.move_face)
+        self.face_timer.start(10)
+
 
         # self.move_timer = QTimer(self)
         # self.move_timer.timeout.connect(self.move_face)
@@ -94,26 +98,18 @@ class Face(QWidget):
             int(0.55 * self.scale),
             int(0.55 * self.scale))
 
+    def mouseMoveEvent(self, event):
+        self.target = event.pos()
 
-    def mouseMoveEvent(self, event: QMouseEvent):
+    def move_face(self):
+        self.step_face()
         self.centro_ojos = QPoint(
-            int(self.c_x + (event.pos().x() - self.c_x) * 0.5),
-            int(self.c_y + (event.pos().y() - self.c_y) * 0.5)
+            int(self.c_x + (self.pose.x() - self.c_x) * 0.5),
+            int(self.c_y + (self.pose.y() - self.c_y) * 0.5)
         )
         self.centro_boca = QPoint(
-            int(self.c_x + (event.pos().x() - self.c_x) * 0.45),
-            int(self.c_y + (event.pos().y() - self.c_y) * 0.45) + int(8 * self.scale)
-        )
-        self.update()
-    
-    def faceDetectionMove(self, facepos):
-        self.centro_ojos = QPoint(
-            int(self.c_x + (facepos[0]*self.width() - self.c_x) * 0.5),
-            int(self.c_y + (facepos[1]*self.height() - self.c_y) * 0.5)
-        )
-        self.centro_boca = QPoint(
-            int(self.c_x + (facepos[0]*self.width() - self.c_x) * 0.45),
-            int(self.c_y + (facepos[1]*self.height() - self.c_y) * 0.45) + int(8 * self.scale)
+            int(self.c_x + (self.pose.x() - self.c_x) * 0.45),
+            int(self.c_y + (self.pose.y() - self.c_y) * 0.45) + int(8 * self.scale)
         )
         self.update()
 
@@ -140,27 +136,22 @@ class Face(QWidget):
             self.speak_count = 100
             self.mouth_size = 1
 
+    def step_face(self):
+        self.pose = QPoint(
+            int(self.pose.x() + (self.target.x() - self.pose.x()) * 0.3),
+            int(self.pose.y() + (self.target.y() - self.pose.y()) * 0.3)
+        )
+        # print(self.pose, self.target)
+
+    def face_detectio_target(self, x, y):
+        self.target = QPoint(
+            int(self.width()  * x * 0.6 + self.width()  * 0.15),
+            int(self.height() * y * 0.8 + self.height() * 0.1)
+        )
+
     def mousePressEvent(self, event: QMouseEvent | None) -> None:
         self.blink_count = 75
 
-    # def move_face(self):
-
-    #     if self.move_ == 0:
-    #         if randint(0, 100) < 10:
-    #             self.move_ = 1
-    #             self.target = QPoint(randint(0, self.width()), randint(int(self.height()*0.4), self.height()))
-
-    #     else:
-
-    #         self.centro_ojos = QPoint(
-    #             int(self.c_x + (event.pos().x() - self.c_x) * 0.5),
-    #             int(self.c_y + (event.pos().y() - self.c_y) * 0.5)
-    #         )
-    #         self.centro_boca = QPoint(
-    #             int(self.c_x + (event.pos().x() - self.c_x) * 0.45),
-    #             int(self.c_y + (event.pos().y() - self.c_y) * 0.45) + int(8 * self.scale)
-    #         )
-    #         self.update()
 
 
 class TextBox(QLabel):
@@ -173,6 +164,10 @@ class TextBox(QLabel):
 
 
 class MainWindow(QMainWindow):
+
+    signal_start_detection = pyqtSignal()
+    signal_stop_detection = pyqtSignal()
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Robotin")
@@ -182,7 +177,6 @@ class MainWindow(QMainWindow):
 
         self.layout = QVBoxLayout()
         self.face = Face()
-        self.face_detection = FaceDetection(self.face)
         self.layout.addWidget(self.face, 3)
 
         self.layout.addWidget(TextBox("sas", self), 1)
@@ -215,69 +209,25 @@ class MainWindow(QMainWindow):
             self.layout.itemAt(2).widget().show()
         if event.key() == Qt.Key.Key_Space:
             self.layout.itemAt(0).widget().speaking = not self.layout.itemAt(0).widget().speaking
-        
-        if self.face_detection.state == "Off":
-            if event.key() == Qt.Key.Key_D: # Cambiamos a deteccion si apretamos D y no estamos en deteccion
-                self.face.setMouseTracking(False)
-                self.face_detection.detect()
-        
-            if event.key() == Qt.Key.Key_S: # Cambiamos a mouse follow si apretamos S y no estamos en deteccion
-                self.face.setMouseTracking(True)
 
-        # Para apagar la deteccion debemos presionar Q imentras estamos en deteccion
-            
+        if event.key() == Qt.Key.Key_D:
+            self.face.setMouseTracking(False)
+            self.signal_start_detection.emit()
+        if event.key() == Qt.Key.Key_S:
+            self.face.setMouseTracking(True)
+            self.signal_stop_detection.emit()
 
-
-class FaceDetection:
-    def __init__(self, robotin):
-        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.robotin = robotin
-        self.device = torch.device("cpu")
-        model = YOLO("best2.pt")
-        self.model = model.to(self.device)
-        self.facepos = None
-        self.state = "Off"
-
-    def detect(self):
-        self.state = "On"
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, frame = cap.read()
-
-            # Realizamos la deteccion
-            results = self.model(frame)
-
-            # Encontramos el bounding box mas grande
-            max_area = 0
-            max_bb = None
-            for result in results[0].boxes:  # Accede a los resultados de las cajas
-                x1, y1, x2, y2 = result.xyxy[0].cpu().numpy().astype(int)
-                area = (x2 - x1) * (y2 - y1)
-                if area > max_area:
-                    max_area = area
-                    max_bb = result
-
-            if max_bb is not None:
-                x1, y1, x2, y2 = max_bb.xyxy[0].cpu().numpy().astype(int)
-                # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                # label = f'{self.model.names[int(max_bb.cls[0].cpu().numpy())]} {max_bb.conf[0].cpu().numpy():.2f}'
-                # cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                # print(f'Centro: {(x1 + x2) / 2}, {(y1 + y2) / 2}')
-                self.robotin.faceDetectionMove((
-                    (640 - ((x1 + x2) / 2)) / 640,
-                    ((y1 + y2) / 2) /480
-                    ))
-
-            # cv2.imshow("frame", frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                cap.release()
-                self.state = "Off"
-                break
 
 if __name__ == "__main__":
+
     app = QApplication(sys.argv)
+
+    facedetection = FaceDetection()
     window = MainWindow()
+
+    window.signal_start_detection.connect(facedetection.start_detection)
+    window.signal_stop_detection.connect(facedetection.stop_detection)
+    facedetection.sender_pose.connect(window.face.face_detectio_target)
+
     window.show()
     sys.exit(app.exec())
